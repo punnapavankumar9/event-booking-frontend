682760cb625f1cdb72e8e153382814db651be61c
punnapavankumar9 <punnapavankumar9@gmail.com>
2025-02-08 19:07:52 +0530

    feat: add reactiveFormsModule to login/register pages

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component, output, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -11,12 +13,33 @@ export class RegisterComponent {
   dialog = output<boolean>({ alias: "closeDialog" });
   signInTab = signal<boolean>(true);
 
+  loginForm = new FormGroup({
+    'username': new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(35)]),
+    'password': new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+
+  registerFrom = new FormGroup({
+    'username': new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(35)]),
+    'firstname': new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(35)]),
+    'lastname': new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(35)]),
+    'password': new FormControl('', [Validators.required, Validators.minLength(8)])
+  })
+
   closeDialog() {
     this.dialog.emit(false);
   }
 
   toggelSignInTab(val: boolean) {
     this.signInTab.set(val);
+  }
+
+  onRegister() {
+    console.log(this.registerFrom.value);
+  }
+
+
+  onSignin() {
+    console.log(this.loginForm.value);
   }
 
 }

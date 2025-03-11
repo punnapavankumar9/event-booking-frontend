ad050068345b1cfc35c636f70fbd8ce48f4644bc
punnapavankumar9 <punnapavankumar9@gmail.com>
2025-03-12 04:13:44 +0530

    feat: add scroll button to home page

--- src/app/features/core/home/home.component.ts
+++ src/app/features/core/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, signal } from '@angular/core';
 import { Movie } from '../types';
 import { MoviesService } from '../../movies/services/movies.service';
 import { MovieCardComponent } from '../../movies/components/movie-card/movie-card.component';
@@ -6,6 +6,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [
     MovieCardComponent,
     RouterLink
@@ -15,15 +16,40 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   movies = signal<Movie[]>([]);
+  @ViewChild('movieContainer') movieContainer!: ElementRef;
+  
+  private scrollAmount = 500; // Approximately two cards width
 
-  constructor(private moviesService: MoviesService) {
-  }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit() {
-    this.moviesService.getMovies(0).subscribe((movies) => {
-      movies.forEach(movie => {
-        this.movies().push(movie);
-      })
-    })
+    this.moviesService.getMovies(0).subscribe({
+      next: (movies) => {
+        this.movies.set(movies);
+      }
+    });
+  }
+
+  scrollMovies(direction: 'left' | 'right') {
+    if (!this.movieContainer) return;
+    
+    const container = this.movieContainer.nativeElement;
+    const scrollAmount = direction === 'left' ? -this.scrollAmount : this.scrollAmount;
+    
+    container.scrollBy({
+      left: scrollAmount,
+      behavior: 'smooth'
+    });
+  }
+
+  canScrollLeft(): boolean {
+    if (!this.movieContainer) return false;
+    return this.movieContainer.nativeElement.scrollLeft > 0;
+  }
+
+  canScrollRight(): boolean {
+    if (!this.movieContainer) return false;
+    const container = this.movieContainer.nativeElement;
+    return container.scrollLeft < (container.scrollWidth - container.clientWidth);
   }
 }
